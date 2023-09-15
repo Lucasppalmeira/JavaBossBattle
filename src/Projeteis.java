950c0187e26872035afa971809661981b6872bcd
Lucasppalmeira <jose.lucas.silva.palmeira@ccc.ufcg.edu.br>
2023-09-14 21:19:04 -0300

    First Projeteis commmit

---
+++ src/Projeteis.java
@@ -0,0 +1,9 @@
+public class Projeteis {
+    private String idIdentificacao;
+    private int amplificacaoDano;
+
+    public void Projeteis(String id, int dano){
+        this.idIdentificacao = id;
+        this.amplificacaoDano = dano;
+    }
+}
