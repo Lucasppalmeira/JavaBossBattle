68b349331430b936cbecb790ee8cc4aff961cd6b
MikasDev <mayke.willyan@estudante.ufcg.edu.br>
2023-09-17 12:33:37 -0300

    movimentação background

--- src/Draw.java
+++ src/Draw.java
@@ -7,15 +7,44 @@ import java.awt.Graphics;
 public class Draw extends JPanel {
 
     private Image background;
+    private int backgroundY;
 
+    // Construtor
     public Draw(String imagePath) {
         ImageIcon imageIcon = new ImageIcon(imagePath);
         background = imageIcon.getImage();
+
+        backgroundY = 0;
+    }
+
+    // Método para mover o fundo
+    public void moveBackground( int dx, int dy){
+        //movendo o fundo para esquerda
+        backgroundY += dx;
+
+        // Se o fundo sair da tela, reinicie
+        if(backgroundY >= getWidth()){
+            backgroundY = 0;
+        }
+        repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+        int imageHeight = background.getHeight(this);
+
+        // Desenhe a imagem de fundo repetidamente
+        int y = backgroundY;
+        while (y < panelHeight) {
+             g.drawImage(background, 0, y, panelWidth, imageHeight, this);
+            y += imageHeight;
+        }
+           
     }
 }
+
+    
--- src/Maingame.java
+++ src/Maingame.java
@@ -1,18 +1,31 @@
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class Maingame {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("BossBattle");
+            JFrame frame = new JFrame("Scrollable Background");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            Draw draw = new Draw("src/sprites/background.jpg");
-            frame.add(draw);
+            Draw backgroundPanel = new Draw("src/sprites/background.jpg");
+            frame.add(backgroundPanel);
 
-            frame.setSize(800, 600); // Defina o tamanho da janela como desejado
+            frame.setSize(800, 600);
             frame.setVisible(true);
+
+            Timer timer = new Timer(20, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    backgroundPanel.moveBackground(2,0);
+                    backgroundPanel.repaint();
+                }
+            });
+            timer.start();
         });
+
 
         Player player = new Player(100, 10);
         System.out.println(player.getVidaPlayer() + " " + player.getDanoPlayer());
